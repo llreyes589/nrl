41da0334f52bbb01a3ba5f6902c86d3200de5cd5
Lester Lou Reyes <clarenista@gmail.com>
2021-12-29 19:30:50 +0800

    update

--- database/seeders/FacilitiesSeeder.php
+++ database/seeders/FacilitiesSeeder.php
@@ -20,14 +20,22 @@ class FacilitiesSeeder extends Seeder
         $firstline = true;
         while (($data = fgetcsv($csvFile, 2000, ",")) !== FALSE) {
             if (!$firstline) {
-                Facility::create([
+                $facility = Facility::create([
                     "region_id" => $data['0'],
                     "accreditation_no" => $data['3'],
                     "name" => utf8_encode($data['4']),
                     "address" => utf8_encode($data['5']),
                     "contact_no" => $data['6'],
                     "email" => $data['7'],
-                ]);    
+                    ]);    
+                $facility->certificate()->create([
+                    'facility_id' => $facility->id,
+                    "or_no" => $data['8'],
+                    "certificate_no" => $data['11'],
+                    "performance" => $data['13'],
+                    "validity" => '2022-12-31',
+                    'key' => md5(microtime())
+                ]);
             }
             $firstline = false;
         }
