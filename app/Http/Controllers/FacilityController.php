b61a2c6423fcf713354d148ccfb274206d1e7384
Lester Lou Reyes <clarenista@gmail.com>
2023-04-04 17:19:05 +0800

    update

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -8,39 +8,43 @@ use App\Models\Region;
 
 class FacilityController extends Controller
 {
-    function index(Request $request){
+    function index(Request $request)
+    {
         $facilities = Facility::with('region_details')->get();
         $regions = Region::all();
-        if($request->filter == 'R'){
+        if ($request->filter == 'R') {
             $facilities = Facility::with('region_details')->where('region_id', $request->region)->get();
             return view('facilities.index', compact('facilities', 'regions'));
-        }else if($request->filter == 'A'){
-            $facilities =  Facility::with('region_details')->where('name', 'like', $request->alphabet.'%')->get();
+        } else if ($request->filter == 'A') {
+            $facilities =  Facility::with('region_details')->where('name', 'like', $request->alphabet . '%')->get();
             return view('facilities.index', compact('facilities', 'regions'));
-            
-        }else{
+        } else {
             $facilities = Facility::with('region_details')->get();
             return view('facilities.index', compact('facilities', 'regions'));
         }
     }
 
-    function create(){
-
+    function create()
+    {
     }
 
-    function edit(Facility $facility){
-
+    function edit(Facility $facility)
+    {
     }
 
-    function destroy(Facility $facility){
+    function destroy(Facility $facility)
+    {
         Facility::destroy($facility->id);
         $facility->deleted_by = \auth()->id();
         $facility->save();
         return redirect()->route('facilities.index')->with('message', 'Facility deleted successfully.')->with('classname', 'alert-danger');
     }
 
-    function createOrUpdate(Facility $facility, Request $request){
+    function createOrUpdate(Facility $facility, Request $request)
+    {
         $req = isset($request->id) ? $request->merge(['updated_by' => auth()->id()])->except('id') : $request->merge(['created_by' => auth()->id()])->except('id');
+        // dd($request->accreditation_no);
+        $validated = $request->validate(['accreditation_no' => 'required|unique:facilities'], ['accreditation_no.unique' => 'The accreditation number has already been taken.']);
         $facility = Facility::updateOrCreate(
             [
                 'id' => $request->id,
@@ -49,5 +53,4 @@ class FacilityController extends Controller
         );
         return redirect()->route('facilities.index')->with('message', 'Facility updated successfully.')->with('classname', 'alert-success');
     }
-
 }
